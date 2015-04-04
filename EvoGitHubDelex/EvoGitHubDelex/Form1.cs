01712959174cbbd33f3bf248222163855cd882de
Daniel <vd_ch2hotmail.com>
2015-04-04 11:27:46 -0500

    Boton funcionando pex

--- EvoGitHubDelex/EvoGitHubDelex/Form1.cs
+++ EvoGitHubDelex/EvoGitHubDelex/Form1.cs
@@ -16,5 +16,11 @@ namespace EvoGitHubDelex
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+            //Victor y Mili
+        }
     }
 }
