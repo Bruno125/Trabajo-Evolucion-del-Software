7106c97ebea6af933b40e0e3f9d9587ad587c855
Bruno Aybar <miguel_bruno@hotmail.com>
2015-04-09 22:05:12 -0500

    Commit bruno

--- EvoGitHubDelex/EvoGitHubDelex/Form1.cs
+++ EvoGitHubDelex/EvoGitHubDelex/Form1.cs
@@ -22,7 +22,7 @@ namespace EvoGitHubDelex
             //Dejen sus messagesboxes confirmando que lo probaron en su casa :D
             MessageBox.Show("MUCHACHOS YA LO PROBÉ EN MI CASA - ALEX MATIAS DEL PIRÚ", "DELEX", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             MessageBox.Show("PROBADO - VICTOR CHUQUIHUACCHA", "TIME IS SO UNKIND", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            
+            MessageBox.Show("POR FIN ME INSTALÉ VISUAL U_U", "BRUNO AYBAR", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             Close();
             
         }
