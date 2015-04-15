1e80b66318d127ebfd8ddf7d4e2f96d12e8e31cc
alexmatiasupc123 <alexmatiasupc123@gmail.com>
2015-04-15 17:43:20 -0500

    probandoMapaSala

--- EvoGitHubDelex/EvoGitHubDelex/Form1.Designer.cs
+++ EvoGitHubDelex/EvoGitHubDelex/Form1.Designer.cs
@@ -28,12 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(124, 176);
+            this.button1.Location = new System.Drawing.Point(42, 416);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -41,14 +45,31 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightGray;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(588, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(156, 464);
+            this.panel1.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Controls.Add(this.button1);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(744, 464);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Sala de Cine";
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -56,6 +77,8 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- EvoGitHubDelex/EvoGitHubDelex/Form1.cs
+++ EvoGitHubDelex/EvoGitHubDelex/Form1.cs
@@ -12,9 +12,56 @@ namespace EvoGitHubDelex
 {
     public partial class Form1 : Form
     {
+
+        List<int> xses;
+        List<int> yses;
+        int w;
+
         public Form1()
         {
             InitializeComponent();
+
+            xses = new List<int>();
+            yses = new List<int>();
+            w = 15;
+
+            generarCoordenadas();
+
+        }
+
+        private void generarCoordenadas()
+        {
+            //ALGORITMO SALVAJE e.e
+
+            int x = 10, y = 10, separadorX = 0, auxX = 0, auxY = 0, separadorY = 0;
+            int numFilas = 10, numCol = 12;
+            
+
+            for (int i = 0; i < numFilas; i++)
+            {
+
+                for (int j = 0; j < numCol; j++)
+                {
+                    if (j != 3 && j != 5)
+                    {
+                        insertarAListas(x + auxX + separadorX, y + auxY + separadorY);
+                        auxX += w;
+                        separadorX = 5;
+                    }
+                }
+
+                auxX = 0;
+                separadorX = 0;
+                separadorY = 5;
+                auxY += w;
+            }
+
+        }
+
+        private void insertarAListas(int x,int y)
+        {
+            xses.Add(x);
+            yses.Add(y);
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -25,6 +72,24 @@ namespace EvoGitHubDelex
             MessageBox.Show("POR FIN ME INSTALÉ VISUAL U_U", "BRUNO AYBAR", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             Close();
             
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Graphics gr = this.CreateGraphics();
+            int Wreal = (int)gr.VisibleClipBounds.Width-panel1.Width;
+            int Hreal = (int)gr.VisibleClipBounds.Height;
+            BufferedGraphicsContext espaciobuffer = BufferedGraphicsManager.Current;
+            BufferedGraphics buffer = espaciobuffer.Allocate(gr, new Rectangle(0, 0, Wreal, Hreal));
+            buffer.Graphics.Clear(this.BackColor);
+            //AQUI VA LOS DIBUJOS
+
+           // buffer.Graphics.FillEllipse(new SolidBrush(Color.Blue),30,30,30,30);
+            //for (int i = 0; i < xses.Count; i++)
+          //      buffer.Graphics.FillRectangle(new SolidBrush(Color.Brown), xses[i], yses[i], w, w);
+
+
+                buffer.Render(gr);
         }
     }
 }
