f8529262ee69a31f9e60fbeb3b709bc30c4d7a88
Bruno Aybar <miguel_bruno@hotmail.com>
2015-05-25 23:46:12 -0500

    Se agrego validacion de funciones que aun no hayan empezado. US001 finalizada

---
+++ EvoGitHubDelex/EvoGitHubDelex/frmFunciones.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace CineEvo.UI
+{
+    partial class frmFunciones
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvFunciones = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSiguiente = new System.Windows.Forms.Button();
+            this.Pelicula = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Horario = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TipoFuncion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Sala = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFunciones)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvFunciones
+            // 
+            this.dgvFunciones.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvFunciones.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Pelicula,
+            this.Horario,
+            this.TipoFuncion,
+            this.Sala});
+            this.dgvFunciones.Location = new System.Drawing.Point(12, 77);
+            this.dgvFunciones.Name = "dgvFunciones";
+            this.dgvFunciones.Size = new System.Drawing.Size(582, 228);
+            this.dgvFunciones.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(21)))), ((int)(((byte)(21)))));
+            this.label2.Location = new System.Drawing.Point(12, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Funciones";
+            // 
+            // btnSiguiente
+            // 
+            this.btnSiguiente.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSiguiente.BackgroundImage = global::CineEvo.UI.Properties.Resources.siguiente;
+            this.btnSiguiente.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnSiguiente.Location = new System.Drawing.Point(451, 315);
+            this.btnSiguiente.Name = "btnSiguiente";
+            this.btnSiguiente.Size = new System.Drawing.Size(142, 50);
+            this.btnSiguiente.TabIndex = 7;
+            this.btnSiguiente.UseVisualStyleBackColor = true;
+            this.btnSiguiente.Click += new System.EventHandler(this.btnSiguiente_Click);
+            // 
+            // Pelicula
+            // 
+            this.Pelicula.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Pelicula.DataPropertyName = "NombrePelicula";
+            this.Pelicula.HeaderText = "Pelicula";
+            this.Pelicula.MinimumWidth = 150;
+            this.Pelicula.Name = "Pelicula";
+            this.Pelicula.ReadOnly = true;
+            // 
+            // Horario
+            // 
+            this.Horario.DataPropertyName = "horario";
+            this.Horario.HeaderText = "Horario";
+            this.Horario.Name = "Horario";
+            this.Horario.ReadOnly = true;
+            // 
+            // TipoFuncion
+            // 
+            this.TipoFuncion.DataPropertyName = "tipo_funcion";
+            this.TipoFuncion.HeaderText = "Tipo de función";
+            this.TipoFuncion.Name = "TipoFuncion";
+            this.TipoFuncion.ReadOnly = true;
+            // 
+            // Sala
+            // 
+            this.Sala.DataPropertyName = "sala";
+            this.Sala.HeaderText = "Sala";
+            this.Sala.Name = "Sala";
+            this.Sala.ReadOnly = true;
+            // 
+            // frmFunciones
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(606, 377);
+            this.Controls.Add(this.btnSiguiente);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dgvFunciones);
+            this.Name = "frmFunciones";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmFunciones";
+            this.Load += new System.EventHandler(this.frmFunciones_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFunciones)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvFunciones;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSiguiente;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Pelicula;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Horario;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TipoFuncion;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Sala;
+    }
+}
---
+++ EvoGitHubDelex/EvoGitHubDelex/frmFunciones.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using CineEvo.BL;
+using CineEvo.DataModel;
+
+namespace CineEvo.UI
+{
+    public partial class frmFunciones : Form
+    {
+        //Metodos
+        FuncionBL objFuncionBL = FuncionBL.ObtenerInstancia();
+        PeliculaBL objPeliculaBL = PeliculaBL.ObtenerInstancia();
+        TipoFuncionBL objTipoFuncionBL = TipoFuncionBL.ObtenerInstancia();
+        SalaBL objSalaBL = SalaBL.ObtenerInstancia();        
+
+        //Atributos
+        public Cine cine;
+
+        /*
+         * Actualizar fechas
+         * update Funcion
+         * set fechaFuncion=GETDATE()
+         */
+
+        public frmFunciones()
+        {
+            InitializeComponent();            
+        }
+
+        public void CargarData()
+        {
+            IList<Funcion> funciones = objFuncionBL.ObtenerFunciones();
+            IList<Pelicula> peliculas = objPeliculaBL.ObtenerPeliculas();
+            IList<TipoFuncion> tipos_funcion = objTipoFuncionBL.ObtenerTiposFuncion();
+            IList<Sala> salas = objSalaBL.ObtenerSalas();
+            DateTime now = DateTime.Now;
+            var data =
+                from p in peliculas
+                join f in funciones on p.idPelicula equals f.idPelicula
+                into pelicula_funcion
+                from pf in pelicula_funcion
+                join t in tipos_funcion on pf.idTipoFuncion equals t.idTipoFuncion
+                into pft
+                join s in salas on pf.idSala equals s.idSala
+                where (pf.fechaFuncion.Date == now.Date
+                        && DateTime.Compare(now,pf.fechaFuncion)<0
+                        && s.idCine == cine.idCine)
+                select new FuncionGridWrapper()
+                {
+                    //-----Capturo-----
+                    id_funcion = pf.idFuncion,
+                    id_pelicula = p.idPelicula,
+                    id_sala = s.idSala,
+                    tipo_funcion = pf.TipoFuncion.nombre,
+                    precio = pf.TipoFuncion.precio,
+                    //-----Capturo-----
+                    NombrePelicula = pf.Pelicula.titulo,
+                    horario = pf.fechaFuncion,
+                    sala = s.nombre
+                };
+            if (data == null)
+            {
+                MessageBox.Show("No existen funciones","Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                dgvFunciones.DataSource = data.ToList();
+            }
+            
+        }
+
+        public class FuncionGridWrapper
+        {            
+            //Orde de los atributos
+            public int id_funcion { get; set; }//0
+            public int id_pelicula { get; set; }//1
+            public int id_sala { get; set; }//2     
+            public double precio { get; set; }//3
+            
+            public string NombrePelicula { get; set; }//4
+            public DateTime horario { get; set; }//5
+            public string tipo_funcion { get; set; }//6
+            public string sala { get; set; }//7
+        }
+
+        private void frmFunciones_Load(object sender, EventArgs e)
+        {
+            //Cargo data
+            CargarData();
+
+            //Titulo
+            this.Text = "Cine: " + cine.nombre.ToString();
+
+            //Oculto lo innecesario
+            dgvFunciones.Columns[0].Visible = false;
+            dgvFunciones.Columns[1].Visible = false;
+            dgvFunciones.Columns[2].Visible = false;
+            dgvFunciones.Columns[3].Visible = false;
+
+            //Ajustar tamaño del dgv
+            dgvFunciones.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+            //Seleccionar toda la fila
+            dgvFunciones.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+        }
+
+        private void btnSiguiente_Click(object sender, EventArgs e)
+        {
+            //Data seleccionada
+            int id_funcion=Int32.Parse(dgvFunciones.SelectedRows[0].Cells[0].Value.ToString());
+            int id_pelicula=Int32.Parse(dgvFunciones.SelectedRows[0].Cells[1].Value.ToString());
+            int id_sala=Int32.Parse(dgvFunciones.SelectedRows[0].Cells[2].Value.ToString());
+            double precio = Double.Parse(dgvFunciones.SelectedRows[0].Cells[3].Value.ToString());
+            string tipo_funcion = dgvFunciones.SelectedRows[0].Cells[6].Value.ToString();
+            
+            //-----Preparo la data-----
+            Funcion funcion = objFuncionBL.ObtenerFuncion(id_funcion);//Testing
+            Pelicula pelicula = objPeliculaBL.ObtenerPelicula(id_pelicula);//Testing
+            Sala sala = objSalaBL.ObtenerSala(id_sala);//Testing
+
+            //-----Creo el formulario-----
+            frmPrecios frmP = new frmPrecios();
+
+            //-----Envio-----
+            frmP.funcion = funcion;
+            frmP.pelicula = pelicula;
+            frmP.sala = sala;
+            frmP.precio = precio;
+            frmP.tipo_funcion = tipo_funcion;
+            frmP.cine = cine;
+
+            //-----Muestro el formulario-----
+            frmP.Show();
+        }
+    }
+}
